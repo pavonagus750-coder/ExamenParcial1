aff8e9134f2f7c1a7066c85b250ffd2b4fbc0aba
pavonagus750-coder <pavonagus750@gmail.com>
2026-02-25 12:20:38 -0600

    Pregunta 5

---
+++ Práctica/pregunta5.sh
@@ -0,0 +1,16 @@
+  312  sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+  313  xcode-select -p
+  314  docker-compose up
+  315  mysql -h 127.0.0.1 -P 3307 -u node -pnode sakila -e "SELECT 1;"
+  316  docker exec -it sklmysql mysql -u root -pr00t
+  317  docker exec -it sklmysql mysql -u root -pr00t
+  318  npm install\nnpm run start
+  319  docker compose up
+  320  docker ps
+  321  docker compose up
+  322  docker ps
+  323  docker exec -it sklmysql bash
+  324  cd ~/Escritorio
+  325  cd ~/Desktop
+  326  touch accept.txt && ls > accept.txt && history | grep " ls" | wc -l >> accept.txt
+  327  cat accept.txt
